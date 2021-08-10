3ae5cb38d3dd35643aed30716a32f0ea8d5175f3
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-08-10 14:09:48 -0300

    Navegação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,4 +14,16 @@ export class HomePage {
 
   }
 
+  login(){
+    //Para navegarmos para a página Categorias utilizamos a injeção de dependência NavController
+    //No TypeScript utilizamos o this para acessar o objeto navCtrl
+    //o push empilha a página em cima da outra em dispositivos móveis funciona dessa forma
+    //Passamos o nome da classe controller como string como já foi definido no Lazy loading
+    //setRoot não mostra a seta para voltar fazemos assim pq após a tela de login não é necessário
+    this.navCtrl.setRoot('CategoriasPage');
+    //this.navCtrl.push('CategoriasPage');
+  
+
+  }
+
 }
